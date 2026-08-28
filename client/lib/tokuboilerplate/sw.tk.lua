d65e5b4eb1bcd323d413d7e87a5446e70904a50a
broma0 <121064585+broma0@users.noreply.github.com>
2026-08-28 13:50:23 -0400

    update sw.tk.lua

--- client/lib/tokuboilerplate/sw.tk.lua
+++ client/lib/tokuboilerplate/sw.tk.lua
@@ -1,15 +1,12 @@
 <%
   serialize = require("santoku.serialize")
   local arr = require("santoku.array")
-  local str = require("santoku.string")
   all_precache = {}
   for _, name in ipairs(public_files_static_for_precache) do
     arr.push(all_precache, name)
   end
   for name in pairs(registered_public_files) do
-    if not str.startswith(name, "splash-") then
-      arr.push(all_precache, name)
-    end
+    arr.push(all_precache, name)
   end
 %>
 
